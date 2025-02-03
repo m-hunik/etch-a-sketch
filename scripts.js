078c12e609fa107c4c800079648f84846061dc5c
airstrike-mike <139953019+airstrike-mike@users.noreply.github.com>
2025-02-03 22:50:25 +0100

    Change number of divs (cells) to 256

--- scripts.js
+++ scripts.js
@@ -4,7 +4,7 @@ div.classList.add("container");
 const scriptTag = document.querySelector("script");
 document.body.insertBefore(div, scriptTag);
 
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < 256; i++) {
   const cell = document.createElement("div");
   cell.classList.add("cell");
   cell.textContent = i + 1;
