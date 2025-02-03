c793b899ace70588cd78a92c153a655b7e63c56f
airstrike-mike <139953019+airstrike-mike@users.noreply.github.com>
2025-02-03 22:18:27 +0100

    Add loop to create 16 divs inside the container

--- scripts.js
+++ scripts.js
@@ -3,3 +3,10 @@ div.classList.add("container");
 
 const scriptTag = document.querySelector("script");
 document.body.insertBefore(div, scriptTag);
+
+for (let i = 0; i < 16; i++) {
+  const cell = document.createElement("div");
+  cell.classList.add("cell");
+  cell.textContent = i + 1;
+  document.body.insertBefore(cell, scriptTag);
+}
